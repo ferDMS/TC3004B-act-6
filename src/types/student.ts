079a0bf2209ce75e22db6c27c7129fbaacbf441c
Daniel Monroy <fernandodms04@gmail.com>
2025-04-03 15:58:54 -0600

    Type definition

---
+++ src/types/student.ts
@@ -0,0 +1,13 @@
+export interface Student {
+  id: String,
+  name: String,
+  status: String,
+  grade: Number,
+  debt: Number,
+};
+
+export interface StudentAPI {
+  id: String,
+  name: String,
+  status: String,
+};
